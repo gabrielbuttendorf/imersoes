93c76f9c0fce014b4452fa0ac9635708ed73b1ca
Gabriel Felipe Buttendorf <bielbuttendorf@gmail.com>
2024-04-10 21:27:36 -0300

    registration date, check in button and input validation

--- nlw-unite/script.js
+++ nlw-unite/script.js
@@ -1,26 +1,51 @@
-const today = dayjs() /* ARRUMAR DAYJS */
+dayjs.extend(window.dayjs_plugin_relativeTime);
+dayjs.locale('pt-br')
 
 function addAttendee() {
+  if (isNameOrEmailEmpty) {
+    alert('Nome e E-mail precisam ser preenchidos.')
+    return
+  }
+
   const tableBody = document.querySelector('tbody')
   const attendeeRow = tableBody.firstElementChild
   
   const newAttendeeName = document.getElementById('full-name').value
   const newAttendeeEmail = document.getElementById('email').value
 
-  // console.log(today.to(dayjs('1990-01-01'))) /* ARRUMAR DAYJS */
-
-  // const newAttendeeRegisterAt = date
+  const newAttendeeRegisterAt = dayjs(randomDate()).fromNow()
   const newAttendee = attendeeRow.cloneNode(true)
 
   const attendeNameElement = newAttendee.querySelector('.attendee-name')
   const attendeEmailElement = newAttendee.querySelector('.attendee-email')
   const attendeRegisterAtElement = newAttendee.querySelector('.attendee-register-at')
-  const attendeCheckInElement = newAttendee.querySelector('.attendee-check-in')
+  const attendeCheckInElement = newAttendee.querySelector('.attendee-check-in .confirm-check-in')
   
   attendeNameElement.textContent = newAttendeeName
   attendeEmailElement.textContent = newAttendeeEmail
-  // attendeRegisterAtElement.textContent = newAttendeeRegisterAt
-  // attendeCheckInElement.textContent = attendee.checkIn
+  attendeRegisterAtElement.textContent = newAttendeeRegisterAt
+  attendeCheckInElement.textContent = 'Confirmar check-in'
   
   tableBody.appendChild(newAttendee)
+  
+  function randomDate() {
+    const startDate = new Date('2024-03-01')
+    const endDate = new Date()
+
+    return new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()))
+  }
+
+  function isNameOrEmailEmpty() {
+    const nameInput = document.getElementById('full-name')
+    const emailInput = document.getElementById('email')
+    
+    return nameInput.value.trim() === '' || emailInput.value.trim() === ''
+  }
 }
+
+const tableBody = document.querySelector('tbody')
+tableBody.addEventListener('click', (event) => {
+  if (event.target.classList.contains('confirm-check-in')) {
+    event.target.textContent = 'Check-in confirmado'
+  }
+})
